4901ff03f45145b9a01ca2c9d90e10893d6aaae9
parsa jiravand <parsajiravand@gmail.com>
2022-01-11 00:24:00 +0330

    update sample

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,39 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import { ref } from "@vue/reactivity";
-import VueClientRecaptcha from "./components/vue-client-recaptcha.vue";
-const val = ref("");
-</script>
-
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <input type="text" v-model="val" />
-  <VueClientRecaptcha
-    :val="val"
-    :show-numbers="true"
-    :showCapitalCaseLetters="false"
-    :showLowerCaseLetters="false"
-    :count="5"
-    :hideLines="false"
-    customTextColor=""
-  />
+  <div class="sample-captcha">
+    <input type="text" v-model="inputValue" />
+
+    <VueClientRecaptcha
+      :value="inputValue"
+      @getCode="getCaptchaCode"
+      @isValid="checkValidCaptcha"
+    />
+  </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+import VueClientRecaptcha from "./components/vue-client-recaptcha.vue";
+export default defineComponent({
+  components: {
+    VueClientRecaptcha,
+  },
+  setup() {
+    /* pass value to captcha  */
+    const inputValue = ref<string|null>(null);
+
+    const getCaptchaCode = (value:string) => {
+      /* you can access captcha code */
+      console.log(value);
+    };
+    const checkValidCaptcha = (value:string) => {
+      /* expected return boolean if your value and captcha code are same return True otherwise return False */
+      console.log(value);
+    };
+    return {
+      inputValue,
+      getCaptchaCode,
+      checkValidCaptcha,
+    };
+  },
+})
+</script>
