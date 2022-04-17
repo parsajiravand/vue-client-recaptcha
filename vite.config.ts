8b77ab95e0229b6768565ba731c4739b3a6e83ca
parsa jiravand <parsajiravand@gmail.com>
2022-04-18 01:31:47 +0430

    setup type declartion

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,23 @@
 const path = require('path');
 const { defineConfig } = require('vite');
 const vue = require('@vitejs/plugin-vue');
+import banner from 'vite-plugin-banner'
+import pkg from "./package.json";
+
 // https://vitejs.dev/config/
+
+const banner = `/*
+ * ${pkg.name}
+ * ${pkg.description}
+ * v${pkg.version}
+ * ${pkg.license} License
+ */
+`;
 export default defineConfig({
 
   build: {
     lib: {
+      banner,
       entry: path.resolve(__dirname, 'src/components/vue-client-recaptcha.vue'),
       name: 'VueClientRecaptcha',
       fileName: (format) => `vue-client-recaptcha.${format}.js`,
