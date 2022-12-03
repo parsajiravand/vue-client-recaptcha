3d6a1630ffa9537ddfce4013756d2f6ecf9ad108
parsa jiravand <parsajiravand@gmail.com>
2022-12-03 03:32:13 +0330

    add module type

--- vite.config.ts
+++ vite.config.ts
@@ -1,38 +1,38 @@
-const path = require('path');
-const { defineConfig } = require('vite');
-const vue = require('@vitejs/plugin-vue');
-import banner from 'vite-plugin-banner'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
+import banner from "vite-plugin-banner";
 import pkg from "./package.json";
-
 // https://vitejs.dev/config/
 
-
 export default defineConfig({
-
   build: {
     lib: {
       banner,
-      entry: path.resolve(__dirname, 'src/components/vue-client-recaptcha.vue'),
-      name: 'VueClientRecaptcha',
+      entry: path.resolve(__dirname, "src/components/vue-client-recaptcha.vue"),
+      name: "VueClientRecaptcha",
       fileName: (format) => `vue-client-recaptcha.${format}.js`,
     },
     rollupOptions: {
-      external: ['vue'],
+      external: ["vue"],
       output: {
         // Provide global variables to use in the UMD build
         // Add external deps here
         globals: {
-          vue: 'Vue',
+          vue: "Vue",
         },
       },
     },
   },
-  plugins: [vue(),banner(`/*
+  plugins: [
+    vue(),
+    banner(`/*
  * ${pkg.name}
  * Creator:${pkg.author}
  * ${pkg.description}
  * v${pkg.version}
  * ${pkg.license} License
  */
-`) ],
+`),
+  ],
 });
